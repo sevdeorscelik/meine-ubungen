38111ab66b31fb22d4515e3f3e4dd4f829c92b17
Sevde <sevdeorscelik@gmail.com>
2022-01-24 13:37:25 +0100

    javascript anfangen 24-01-2022

---
+++ 24-01-2022/javascript-anfangen/index.js
@@ -0,0 +1,39 @@
+// Erstes Hello World Program
+
+console.log("Hello World!");
+
+// Erste Rechnung mit Addition
+let add = 5+6;
+
+
+// Erste Rechnung mit Multiplikation
+let mult = 3 * 4 ;
+
+
+// Erste Rechnung mit Subtration
+let sub = 10 - 5 ;
+
+
+// Erste Rechnung mit Division
+let div = 5 / 2 ;
+
+
+// Aufgabe auf der Konsole
+console.log("Addition", add);
+console.log("Multiplikation", mult);
+console.log("Subtration", sub);
+console.log("Division", div); 
+
+
+
+let startkapital = 1000 ;
+let aufschlag = 1.015 ;
+let bonus = 50 ;
+let laufzeit = 30 ;
+
+let endsumme = ( startkapital * aufschlag ** laufzeit ) + bonus ;
+console.log(endsumme);
+
+
+
+
