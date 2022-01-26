70c2211dc103eac9741cae73031c1fd90ee14fca
Sevde <sevdeorscelik@gmail.com>
2022-01-26 11:39:38 +0100

    add array

---
+++ 26-01-2022/index.js
@@ -0,0 +1,20 @@
+
+const greetings = "Hello World! Have a nice Wednesday! Wednesday is a working day.";
+
+//Property length
+
+// console.log(greetings.length);  // keine klammer 
+
+
+//Methode toUpperCase()
+const allCapitalLetters = greetings.toUpperCase() ;
+
+//Methode toLowerCase()
+const allSmallLetters = greetings.toLowerCase() ;
+
+
+//includes()
+const contains = greetings.toLowerCase().includes("wednesday is a".toLowerCase()) ;
+console.log(contains);
+
+
