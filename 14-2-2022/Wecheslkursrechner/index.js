6411361dcf8692989b39fd97c35129d3647903d4
Sevde <sevdeorscelik@gmail.com>
2022-02-14 13:54:06 +0100

    calcWechselKur

---
+++ 14-2-2022/Wecheslkursrechner/index.js
@@ -0,0 +1,16 @@
+
+
+const printHello = () => {
+    console.log("Hallo WELT!");
+
+    //Hello World in das Element greeting schreiben 
+    document.getElementById("greeting").innerHTML = "Hello World"
+}
+
+const calcDollar = () => {
+    // Find den Betrag des Input Feldes
+    const euro = document.getElementById("euroBetrag").value;
+
+    const dollar = euro * 1.4;
+    document.getElementById("dollarValue").innerHTML = dollar;
+}
