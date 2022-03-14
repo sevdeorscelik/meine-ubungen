1409f5e5f3632cb22d4a03430f127de5e01466db
Sevde <sevdeorscelik@gmail.com>
2022-03-14 11:39:57 +0100

    flashcard with Edward

--- 14-3-2022-DOM-React/live-coding/js/main.js
+++ 14-3-2022-DOM-React/live-coding/js/main.js
@@ -1,4 +1,18 @@
+// import { capitalizeFirstCharacter, getTodaysDate } from './tools.js';
+
+import * as  tools from './tools.js';
+import { flashcards } from './data/flashcards.js'
+
 const contentElem = document.querySelector('.content');
 
-contentElem.innerHTML = 'test';
+contentElem.innerHTML = `
+<p>${tools.capitalizeFirstCharacter('This is the title of the article')}</p>
+<p>${tools.getTodaysDate()}</p>
+<div classNem= "flashcards">
+    ${flashcards.map(flashcards => {
+        return '<div>nnn</div>';
+    }).join('')}
+
+</div>
+` ;
 
