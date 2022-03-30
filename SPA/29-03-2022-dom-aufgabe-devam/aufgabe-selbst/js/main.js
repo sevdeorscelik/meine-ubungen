f8880d07d3254c4f7de141cc910d78642c32d349
Sevde <sevdeorscelik@gmail.com>
2022-03-30 10:09:59 +0200

    game end

--- SPA/29-03-2022-dom-aufgabe-devam/aufgabe-selbst/js/main.js
+++ SPA/29-03-2022-dom-aufgabe-devam/aufgabe-selbst/js/main.js
@@ -6,17 +6,19 @@ const catElem = document.querySelector('#cat')
 const sunElem = document.querySelector('.sun-icon')
 const sunTextElem =document.querySelector('.sun-text')
 
+
+
 carElem.addEventListener('dragstart', (e) => {
     
-    e.dataTransfer.setData('car', carElem.id);
+    e.dataTransfer.setData('icon', carElem.id);
 })
 
 catElem.addEventListener('dragstart', (e)=>{
-    e.dataTransfer.setData('cat',catElem.id )
+    e.dataTransfer.setData('icon',catElem.id )
 })
 
 sunElem.addEventListener('dragstart', (e) => {
-    e.dataTransfer.setData('sun', sunElem.id)
+    e.dataTransfer.setData('icon', sunElem.id)
 }) 
 
 
@@ -39,28 +41,40 @@ sunTextElem.addEventListener('dragover', (e) => {
 carTextElem.addEventListener('drop', (e) => {
     e.preventDefault()
 
-    let id = e.dataTransfer.getData('car')
-    
-    
+    let id = e.dataTransfer.getData('icon')
     carTextElem.appendChild(document.getElementById(id))
-
-    carTextElem.style.backgroundColor = 'green'
+    
+    if(carTextElem.firstChild.id === carElem.id){
+        carTextElem.style.backgroundColor = 'green'
+    } else{
+        carTextElem.style.backgroundColor = 'brown'
+    }
  
 })
 
 catTextElem.addEventListener('drop', (e) => {
     e.preventDefault()
-    let id = e.dataTransfer.getData('cat')
+    let id = e.dataTransfer.getData('icon')
     
     catTextElem.appendChild(document.getElementById(id))
-    catTextElem.style.backgroundColor = 'green'
-    
+
+    if(catTextElem.firstChild.id === catElem.id){
+        catTextElem.style.backgroundColor = 'green'
+    } else{
+        catTextElem.style.backgroundColor = 'brown'
+    }    
 })
 
 sunTextElem.addEventListener('drop', (e) => {
     e.preventDefault()
-    let id = e.dataTransfer.getData('sun')
+    let id = e.dataTransfer.getData('icon')
 
     sunTextElem.appendChild(document.getElementById(id))
-    sunTextElem.style.backgroundColor = 'green'
+
+    if(sunTextElem.firstChild.id === sunElem.id){
+        sunTextElem.style.backgroundColor = 'green'
+    } else{
+        sunTextElem.style.backgroundColor = 'brown'
+    }
+   
 })
