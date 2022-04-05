abd19ad3a941689ebbcf8b5d9384d75e47e34da8
Sevde <sevdeorscelik@gmail.com>
2022-04-05 10:11:27 +0200

    window.onresize

---
+++ SPA/5-4-2022/vite-project/main.js
@@ -0,0 +1,37 @@
+import './style.css'
+
+
+const text = '<div><strong>This text is important</strong> ajhfjsfgjsadgfjdshgfld <strong>This text is important</strong> jshgfljhgflsdf <strong>This text is important</strong></div>'
+document.querySelector('#app').innerHTML = `
+  <h1>Highlight Strong Tags!</h1>
+  <div class = "text">${text}</div>
+  <button class="btnChange">Change Color of Strong Tags</button>
+  <br><hr><br>
+  <div class='windowInfo'></div>
+`
+const btnChangeElem = document.querySelector('.btnChange')
+
+btnChangeElem.addEventListener('click', () => {
+  const strongElems = document.querySelectorAll('strong')
+  strongElems.forEach(strongElem => {
+    if(strongElem.style.color === 'black'){
+      strongElem.style.color = 'red'
+    }else if(strongElem.style.color === 'red') {
+      strongElem.style.color = 'green'
+    }else{
+      strongElem.style.color = 'black'
+    }
+    
+  })
+  })
+
+
+  window.onresize = () => {
+    const windowInfoElem = document.querySelector('.windowInfo')
+    const w = document.documentElement.clientWidth
+    const h = document.documentElement.clientHeight
+
+    windowInfoElem.innerHTML = `width: ${w}px, height: ${h}px`
+  }
+ 
+
