0aa8e278661ac7982c5d8857487e4069a0cbc247
Sevde <sevdeorscelik@gmail.com>
2022-04-06 11:48:21 +0200

    delete

--- SPA/6-4-2022 - promise/promise-live-codding/todo-list-with-json/main.js
+++ SPA/6-4-2022 - promise/promise-live-codding/todo-list-with-json/main.js
@@ -2,25 +2,75 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Todo App</h1>
-  <form>
-    <div class="row">
-      <input type="text/>
-        <button class="btnAdd"> +Add Item</button>
-    </div>
-    <div class="tasks"></div>
-  </form>
-`
+<form>
+  <div class="row">
+    <input class="text" type="text" /> 
+    <button class="btnAdd">Add Item</button>
+  </div>
+  <div class="tasks"></div>
+</form>
+`;
 
-const textElem = document.querySelector('.text')
-const btnAddElem = document.querySelector('.btnAdd')
-const tasksElem = document.querySelector('.tasks')
+const textElem = document.querySelector('.text');
+const btnAddElem = document.querySelector('.btnAdd');
+const tasksElem = document.querySelector('.tasks');
 
-const renderList = () => {
-  const taskElem = document.createElement('div')
-  taskElem.innerHTML = 'this is a task'
-  taskElem.classList.add('task')
-  tasksElem.appendChild(taskElem)
-}  
+const getTasks = async () => {
+  const response = await fetch('http://localhost:5011/todos');
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
 
-renderList()
+const renderList = async () => {
+  tasksElem.innerHTML = '';
+  const tasks = await getTasks();
+  tasks.forEach(task => {
+    // const taskElem = document.createElement('div');
+    // taskElem.innerHTML = task.text;
+    // taskElem.classList.add('task');
+    // tasksElem.appendChild(taskElem);
 
+    const taskElem = document.createElement('div');
+    taskElem.setAttribute('id', task.id);
+    taskElem.innerHTML = `
+   <input type="checkbox"/> 
+   <div>${task.text}</div>
+   <button class="btnDelete">delete</button>
+    `;
+    taskElem.classList.add('task');
+    tasksElem.appendChild(taskElem);
+  });
+  textElem.value = '';
+  textElem.focus();
+  const deleteButtonElems = document.querySelectorAll('.btnDelete');
+  deleteButtonElems.forEach(m => m.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const currentTaskElem = m.parentElement;
+    const currentId = currentTaskElem.getAttribute('id');
+    const requestOptions = {
+      method: 'DELETE'
+    };
+    const response = await fetch(`http://localhost:5011/todos/${currentId}`, requestOptions);
+    renderList();
+  }));
+};
+
+renderList();
+
+btnAddElem.addEventListener('click', async (e) => {
+  e.preventDefault();
+
+  const newTodo = {
+    "text": textElem.value,
+    "finished": false
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: { "Content-type": "application/json; charset=UTF-8" }
+  };
+  const response = await fetch('http://localhost:5011/todos', requestOptions);
+  renderList();
+});
