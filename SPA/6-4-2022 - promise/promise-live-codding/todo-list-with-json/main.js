0bd7035480f6ff9c83b3f96a62d6202fe3752223
Sevde <sevdeorscelik@gmail.com>
2022-04-06 10:07:20 +0200

    json todo app

---
+++ SPA/6-4-2022 - promise/promise-live-codding/todo-list-with-json/main.js
@@ -0,0 +1,26 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Todo App</h1>
+  <form>
+    <div class="row">
+      <input type="text/>
+        <button class="btnAdd"> +Add Item</button>
+    </div>
+    <div class="tasks"></div>
+  </form>
+`
+
+const textElem = document.querySelector('.text')
+const btnAddElem = document.querySelector('.btnAdd')
+const tasksElem = document.querySelector('.tasks')
+
+const renderList = () => {
+  const taskElem = document.createElement('div')
+  taskElem.innerHTML = 'this is a task'
+  taskElem.classList.add('task')
+  tasksElem.appendChild(taskElem)
+}  
+
+renderList()
+
