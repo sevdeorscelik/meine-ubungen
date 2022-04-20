54e5c9b907088fa17ecf242a584bf2dc5ee5a183
Sevde <sevdeorscelik@gmail.com>
2022-04-20 09:43:31 +0200

    react-component

---
+++ SPA/20-04-2022-components/test-basic-jsx/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+
+
+function App() {
+  const title = 'Info Site'
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      hello
+    </div>
+  )
+}
+
+export default App
