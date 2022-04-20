0f465ecbf2b6998345bd392c406cd6a46afa12f9
Sevde <sevdeorscelik@gmail.com>
2022-04-20 10:49:51 +0200

    basic jpx

--- SPA/20-04-2022-components/test-basic-jsx/src/App.jsx
+++ SPA/20-04-2022-components/test-basic-jsx/src/App.jsx
@@ -4,12 +4,29 @@ import './App.scss'
 
 function App() {
   const title = 'Info Site'
+  const colors = ['red', 'green', 'yellow', 'orange'];
+  //const colors =[]
+
   return (
     <div className="App">
       <h1>{title}</h1>
-      hello
+      Colors:
+      {colors.length > 0 && (
+        <ul>
+          {colors.map(color => {
+            return (
+              <li>{color}</li>
+            )
+          })
+          }
+        </ul>
+
+      )}
+
     </div>
   )
 }
 
 export default App
+
+//14.satirdaki kod, colors arrayi bos oldugunda liste göstermeyecek.
