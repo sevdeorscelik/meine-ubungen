4832a496fed7aec4f847debc436e246f37f75827
Sevde <sevdeorscelik@gmail.com>
2022-05-11 11:18:02 +0200

    styling

--- SPA/COMPONENT/spa-tasks/src/pages/menu.jsx
+++ SPA/COMPONENT/spa-tasks/src/pages/menu.jsx
@@ -13,11 +13,12 @@ const Menu = () => {
             <div className="container-menu">
                 <h4>Single Page Application :</h4>
 
-                <div><NavLink to="/spa002"><strong>Spa002</strong> </NavLink>- Display object with photo and NavLinknk</div>
-                <div><NavLink to="/spa004"><strong>Spa004</strong> </NavLink>- Display data from an array and a nested array in JSX</div>
-                <div><NavLink to="/spa005"><strong>Spa005</strong> </NavLink>- Update the page based on text typed into input fields</div>
-                <div><NavLink to="/spa006"><strong>Spa006</strong> </NavLink>- Move element up on button cNavLinkck</div>
-                <div><NavLink to="/spa007"><strong>Spa007</strong> </NavLink>- Load data from the Website API</div>
+                <div><NavLink to="/spa002"><strong>Spa002</strong> - Display object with photo and link </NavLink></div>
+                <div><NavLink to="/spa004"><strong>Spa004</strong> - Display data from an array and a nested array in JSX </NavLink></div>
+                <div><NavLink to="/spa005"><strong>Spa005</strong> - Update the page based on text typed into input fields </NavLink></div>
+                <div><NavLink to="/spa006"><strong>Spa006</strong> - Move element up on button click </NavLink></div>
+                <div><NavLink to="/spa007"><strong>Spa007</strong> - Load data from the Website API
+ </NavLink></div>
 
                 
                
